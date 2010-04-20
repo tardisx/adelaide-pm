b618d1cde3974c7a56bb6b2fa2a30e6d7651aa6a
Justin Hawkins <justin@hawkins.id.au>
2010-04-20 05:54:31 +0000

    New smoker. Updated tests.

---
+++ bin/smoke.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use File::Temp qw/tempfile/;
+
+my ($fh, $filename) = tempfile(  TEMPLATE => 'tempXXXXX',
+                                 SUFFIX => '.tar.gz');
+warn $filename;
+my $revision = `git show master^ | grep commit | perl -pne 's/\n//s;'`;
+$revision =~ s/^commit\s+//;
+
+my $platform = `uname`;
+chomp $platform;
+
+system ("/usr/bin/prove","--archive",$filename);
+system ("/usr/local/bin/smolder_smoke_signal","--server","perlcode.info","--port","8008","--username","justin","--password","foobar","--file", $filename,  "--project","adelaidepm", "--revision", $revision, "--platform", $platform);
+
+unlink $filename;
+
--- t/03-adelaide-tech.t
+++ t/03-adelaide-tech.t
@@ -2,12 +2,12 @@ use Test::More tests => 5;
 
 use lib 'lib';
 
-use_ok 'PM::Meeting::Dates::Adelaide::Social';
+use_ok 'PM::Meeting::Dates::Adelaide::Tech';
 
-my $obj = PM::Meeting::Dates::Adelaide::Social->new();
+my $obj = PM::Meeting::Dates::Adelaide::Tech->new();
 
 ok (defined $obj, 'object exists');
-ok ($obj->isa('PM::Meeting::Dates::Adelaide::Social'), 'right kind');
+ok ($obj->isa('PM::Meeting::Dates::Adelaide::Tech'), 'right kind');
 
 ok (defined $obj->next_meeting(), 'can call next_meeting');
 ok ($obj->next_meeting()->isa('PM::Meeting'), 'isa PM::Meeting');
