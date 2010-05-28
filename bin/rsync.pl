de608bec6e58e5fb33b65b742948390f76cddc9a
Justin Hawkins <justin@hawkins.id.au>
2010-05-28 13:34:17 +0930

    Add verbose argument.

--- bin/rsync.pl
+++ bin/rsync.pl
@@ -2,14 +2,20 @@
 
 use strict;
 use warnings;
-
 use Getopt::Long;
-my $upload;
-my $result = GetOptions ("upload" => \$upload);
+
+my $upload;   # shall we upload this?
+my $verbose;  # shall we use verbose ttree?
+
+my $result = GetOptions (upload   => \$upload,  
+                         verbose  => \$verbose,);
 die "bad args" unless $result;
 
+my @verbose_args;
+@verbose_args = ('--verbose') if ($verbose);
+
 chdir 'web' || die $!;
-system ('/usr/local/bin/ttree', '--EVAL_PERL', '-f', 'lib/site.cfg', '-a');
+system ('/usr/local/bin/ttree', '--EVAL_PERL', '-f', 'lib/site.cfg', '-a', @verbose_args);
 
 system('rsync', '--delete', '-qa',  'html/', '/home/justin/web/adelaidepm/')
   if $upload;
